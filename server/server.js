74f33008b2a7a64837d8eb9bd788174b667a6e34
ThomyQ1 <quevedothomasleonel@gmail.com>
2024-02-05 16:45:49 -0300

    arregle el error de nodemon

--- server/server.js
+++ server/server.js
@@ -9,35 +9,14 @@ import morgan from "morgan";
 import { engine } from "express-handlebars";
 import { createServer } from "http";
 import { Server } from "socket.io";
-import productsManager from "./src/data/fs/product.fs.js";
-
 
 const server = express();
 const PORT = 8080;
-const ready = () => console.log("Server ready on port: " + PORT);
+const ready = () => console.log("Servidor listo en el puerto: " + PORT);
 const httpServer = createServer(server);
 const socketServer = new Server(httpServer);
-httpServer.listen(PORT, ready);
-socketServer.on("connection", (socket) => {
-  socket.emit("welcome", "welcome to my ecommerce");
-  socket.emit("products", productsManager.read());
-  socket.on("new product", async (data) => {
-    try {
-      console.log(data);
-      await productsManager.create(data);
-      socketServer.emit("products", productsManager.read());
-    } catch (error) {
-      console.log(error);
-    }
-  });
-});
 
-server.engine("handlebars", engine());
-server.set("view engine", "handlebars");
-server.set("views", __dirname + "/src/views");
-
-server.listen(PORT, ready);
-
+// Configuración de middlewares y rutas
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 server.use(express.static(__dirname + "/public"));
@@ -45,3 +24,26 @@ server.use(morgan("dev"));
 server.use("/", router);
 server.use(errorHandler);
 server.use(pathHandler);
+
+// Configuración de Socket.IO
+socketServer.on("connection", (socket) => {
+  socket.emit("welcome", "Bienvenido a mi ecommerce");
+  socket.emit("productos", productsManager.read());
+  socket.on("nuevo producto", async (data) => {
+    try {
+      console.log(data);
+      await productsManager.create(data);
+      socketServer.emit("productos", productsManager.read());
+    } catch (error) {
+      console.log(error);
+    }
+  });
+});
+
+// Configuración del motor de vistas
+server.engine("handlebars", engine());
+server.set("view engine", "handlebars");
+server.set("views", __dirname + "/src/views");
+
+// Iniciar el servidor
+httpServer.listen(PORT, ready);
